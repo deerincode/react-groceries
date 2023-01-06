a1aba991a4ed5dd092ab53c2d3e44af2a2addc4e
Vogue Doesong <nboney@email.sc.edu>
2023-01-06 15:02:18 -0500

    Assignment requiresments completed

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        item: 'English Muffins',
+        brand: 'Thomas',
+        units: 'pack',
+        quantity: 6,
+        isPurchased: false
+      },
+      {
+        item: 'Mint Yerba Mate',
+        brand: 'Guayaki',
+        units: 'can',
+        quantity: 1,
+        isPurchased: false
+      },
+      {
+        item: 'Milk',
+        brand: 'Happy Pastures',
+        units: 'gallon',
+        quantity: 1,
+        isPurchased: false
+      },
+      {
+        item: 'Bananas',
+        brand: 'Dole',
+        units: 'bunch',
+        quantity: 1,
+        isPurchased: false
+      },
+
+]
